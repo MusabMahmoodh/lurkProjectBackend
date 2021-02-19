5ce4f79e29cd990db9290ba1c15a6d337679deea
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-02-19 12:01:05 +0530

    pagination and search

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
@@ -8,11 +10,30 @@ from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def getProducts(request):
+    query = request.query_params.get('keyword')
+    if query == None:
+        query = ''
 
-    products = Product.objects.all()
+    products = Product.objects.filter(
+        name__icontains=query).order_by('-createdAt')
+
+    page = request.query_params.get('page')
+    paginator = Paginator(products, 2)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    if page == None:
+        page = 1
+
+    page = int(page)
+    print('Page:', page)
     serializer = ProductSerializer(products, many=True)
-
-    return Response(serializer.data)
+    return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
 
 
 @api_view(['GET'])
